4ab8e0f54b0f33869ac2fd4cdc64ff58a3b963f8
iexus <2.-@twopointline.com>
2014-09-10 19:12:13 +0100

    Remove un-needed spec

--- tests/SanitySpec.js
+++
@@ -1,6 +0,0 @@
-describe("A sanity check", function(){
-    it ("will let us know if the world is ok.", function(){
-        expect(true).toBe(true);
-        expect(false).toBe(false);
-    });
-});
